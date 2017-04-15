de963d470716654f88bf046ed496a7d11a6edebd
glewczuk <lewczuk.grzeg@gmail.com>
2017-04-15 11:49:05 +0200

    create Lexer based on token descritpions

--- lexer/Lexer.py
+++ lexer/Lexer.py
@@ -1,49 +1,51 @@
-from token.Token import Token
-from token.TokenType import TokenType
+from tokens.Token import Token
+from tokens.Tokenizer import Tokenizer
 
 
-class Scanner:
-    current_word = 0
-
-    def __init__(self, file_name):
-        self.words = self.__open_file_and_read_words(file_name)
-
-    def __open_file_and_read_words(self, file_name):
-        file = open(file_name).read()
-
-        for splitter in TokenType.OPERATOR.keys():
-            file = file.replace(splitter, ' ' + splitter + ' ')
-        for splitter in TokenType.PARENTHESIS.keys():
-            file = file.replace(splitter, ' ' + splitter + ' ')
-
-        return file.split()
+class Lexer:
+    def __init__(self, file_reader):
+        self.__file_reader = file_reader
+        self.__tokenizer = Tokenizer()
 
     def get_token(self):
-        token = None
-
-        if self.current_word < len(self.words):
-            word = self.__next_word()
-            token_type = self.__tokenize_word(word)
-            token = Token(token_type, word)
+        self.__ignore_whitespaces()
+        token = self.__read_token()
 
         return token
 
-    def __next_word(self):
-        word = self.words[self.current_word]
-        self.current_word += 1
+    def __ignore_whitespaces(self):
+        next_char = self.__file_reader.next_byte()
 
-        return word
+        while next_char and next_char.isspace():
+            self.__file_reader.read_byte()
+            next_char = self.__file_reader.next_byte()
 
-    def __tokenize_word(self, word):
-        if word in TokenType.PARENTHESIS.keys():
-            return TokenType.PARENTHESIS[word]
-        elif word in TokenType.OPERATOR.keys():
-            return TokenType.OPERATOR[word]
-        elif word in TokenType.DATA_TYPE.keys():
-            return TokenType.DATA_TYPE[word]
-        elif word in TokenType.EXPRESSION_CONTROLLER.keys():
-            return TokenType.EXPRESSION_CONTROLLER[word]
-        elif word.isdigit() and not (len(word) > 1 and word[0] == '0'):
-            return TokenType.NUMBER['NAME']
-        elif word.isalnum() and word[0].isalpha():
-            return TokenType.IDENTIFIER['NAME']
+    def __read_token(self):
+        token_list = self.__tokenizer.descriptors
+
+        next_char = self.__file_reader.next_byte()
+        if not next_char:
+            return None
+
+        word = ''
+        possible_tokens = [token for token in token_list
+                           if token.qualifier(next_char, len(word), token.token_type.value)]
+
+        while possible_tokens:
+            token_list = possible_tokens
+            word += self.__file_reader.read_byte()
+            next_char = self.__file_reader.next_byte()
+            possible_tokens = [token for token in token_list
+                               if token.qualifier(next_char, len(word), token.token_type.value)]
+
+        token_list = [token for token in token_list if token.acceptor(word, token.token_type.value)]
+
+        if not token_list:
+            raise Exception("zly znak")
+
+        tokena = token_list[0]
+
+        if not next_char.isspace() and tokena.required_space and tokena not in self.__tokenizer.tokens_required_space:
+            raise Exception("spacja")
+
+        return Token(tokena.token_type, word, 0, 0)
---
+++ tokens/Tokenizer.py
@@ -0,0 +1,71 @@
+from tokens.TokenDescriptor import TokenDescriptor
+from tokens.TokenType import TokenType
+
+
+class Tokenizer:
+    # (token_type, qualifier, required_space, acceptor)
+    __token_descriptors = [
+        TokenDescriptor(TokenType.EQUAL,
+                        required_space=False),
+        TokenDescriptor(TokenType.NOT_EQUAL,
+                        required_space=False),
+        TokenDescriptor(TokenType.GREATER,
+                        required_space=False),
+        TokenDescriptor(TokenType.LESS,
+                        required_space=False),
+        TokenDescriptor(TokenType.GREATER_OR_EQUAL,
+                        required_space=False),
+        TokenDescriptor(TokenType.LESS_OR_EQUAL,
+                        required_space=False),
+        TokenDescriptor(TokenType.ASSIGN,
+                        required_space=False),
+        TokenDescriptor(TokenType.PARAMETRIZE,
+                        required_space=False),
+
+        TokenDescriptor(TokenType.LEFT_BRACKET,
+                        required_space=False),
+        TokenDescriptor(TokenType.RIGHT_BRACKET,
+                        required_space=False),
+        TokenDescriptor(TokenType.SQUARE_LEFT_BRACKET,
+                        required_space=False),
+        TokenDescriptor(TokenType.SQUARE_RIGHT_BRACKET,
+                        required_space=False),
+        TokenDescriptor(TokenType.CLIP_LEFT_BRACKET,
+                        required_space=False),
+        TokenDescriptor(TokenType.CLIP_RIGHT_BRACKET,
+                        required_space=False),
+
+        TokenDescriptor(TokenType.UNIT),
+        TokenDescriptor(TokenType.BITSTRING),
+        TokenDescriptor(TokenType.BOOL),
+        TokenDescriptor(TokenType.ARRAY),
+        TokenDescriptor(TokenType.CHOICE),
+        TokenDescriptor(TokenType.SEQUENCE),
+
+        TokenDescriptor(TokenType.AND),
+        TokenDescriptor(TokenType.OR),
+        TokenDescriptor(TokenType.DEFAULT),
+
+        TokenDescriptor(TokenType.TRUE),
+        TokenDescriptor(TokenType.FALSE),
+
+        # non-standard TokenDescriptions always at end!
+        TokenDescriptor(TokenType.NUMBER_LITERAL,
+                        qualifier=lambda char, *args: char.isdigit(),
+                        acceptor=lambda word, *args: word.isdigit()),
+        TokenDescriptor(TokenType.IDENTIFIER,
+                        qualifier=lambda char, char_position, *args: char.isalnum() if char_position else char.isalpha(),
+                        acceptor=lambda word, *args: word.isalnum()),
+    ]
+
+    def __init__(self):
+        self.__tokens_required_space = [token_desc for token_desc in self.__token_descriptors
+                                        if token_desc.required_space]
+
+    @property
+    def descriptors(self):
+        return self.__token_descriptors
+
+    @property
+    def tokens_required_space(self):
+        return self.__tokens_required_space
